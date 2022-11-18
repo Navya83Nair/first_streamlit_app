2d3a73c1bc769ec2fea2ea97d582596cbca56600
Navya83Nair <118523899+Navya83Nair@users.noreply.github.com>
2022-11-18 19:57:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-print(emoji.emojize(":grinning_squinting_face:"))
+streamlit.print(emoji.emojize(":grinning_squinting_face:"))
 streamlit.header(' Breakfast Menu  ')
 streamlit.write('Banana','Apple', 'Chikku', 'Milk')  
 streamlit.text('Curd and Muesli')
