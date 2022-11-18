7b2d9a858cbe3f92143127e776f76eb02a121e08
Navya83Nair <118523899+Navya83Nair@users.noreply.github.com>
2022-11-18 20:15:26 +0530

    Update streamlit_app.py
    read file and display

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header(' Breakfast Menu  ')
 streamlit.write('Banana','Apple', 'Chikku', 'Milk')  
 streamlit.text('Curd and Muesli')
 streamlit.header('🍌 :zap: Build Your Own Fruit Smoothie :smile: 🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
