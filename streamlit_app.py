8b72a380dc849e730cb4ae40b66e66db0e67ffaa
Navya83Nair <118523899+Navya83Nair@users.noreply.github.com>
2022-11-18 19:41:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.write('Banana','Apple')  
+streamlit.write('Banana','Apple', 'Chikku', 'Milk')  
 streamlit.text('Curd and Muesli')
 
