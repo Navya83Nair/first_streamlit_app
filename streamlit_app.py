e0e68852897b64f7deca8bfdbb34d15193b0dc70
Navya83Nair <118523899+Navya83Nair@users.noreply.github.com>
2022-12-07 00:11:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header(' Breakfast Menu  ')
 streamlit.write('Banana','Apple', 'Chikku', 'Milk')  
@@ -15,3 +17,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
